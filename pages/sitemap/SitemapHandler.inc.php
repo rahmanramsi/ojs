9b782c8dcc5526ca5d8a4e631237c6315737a355
Rahman Ramsi <rahmanramsi19@gmail.com>
2026-02-18 14:24:32 +0800

    improve sitemap menggunakan raw query

--- pages/sitemap/SitemapHandler.inc.php
+++ pages/sitemap/SitemapHandler.inc.php
@@ -31,26 +31,71 @@ class SitemapHandler extends PKPSitemapHandler {
 		$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'search')));
 		// Issues
 		$issueDao = DAORegistry::getDAO('IssueDAO'); /* @var $issueDao IssueDAO */
-		$galleyDao = DAORegistry::getDAO('ArticleGalleyDAO'); /* @var $galleyDao ArticleGalleyDAO */
 		if ($journal->getData('publishingMode') != PUBLISHING_MODE_NONE) {
 			$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'issue', 'current')));
 			$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'issue', 'archive')));
-			$publishedIssues = $issueDao->getPublishedIssues($journalId);
-			while ($issue = $publishedIssues->next()) {
-				$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'issue', 'view', $issue->getId())));
-				// Articles for issue
-				$submissionsIterator = Services::get('submission')->getMany([
-					'issueIds' => [$issue->getId()],
-					'contextId' => $journal->getId(),
-					'status' => STATUS_PUBLISHED,
-				]);
-				foreach($submissionsIterator as $submission) {
-					// Abstract
-					$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'article', 'view', array($submission->getBestId()))));
-					// Galley files
-					$galleys = $galleyDao->getByPublicationId($submission->getCurrentPublication()->getId());
-					while ($galley = $galleys->next()) {
-						$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'article', 'view', array($submission->getBestId(), $galley->getBestGalleyId()))));
+
+			$publishedIssuesResult = $issueDao->retrieve(
+				'SELECT i.issue_id
+				FROM issues i
+				LEFT JOIN custom_issue_orders o ON (o.issue_id = i.issue_id)
+				WHERE i.journal_id = ? AND i.published = 1
+				ORDER BY o.seq ASC, i.current DESC, i.date_published DESC',
+				[(int) $journalId]
+			);
+			$issueIds = [];
+			while ($issueRow = $publishedIssuesResult->current()) {
+				$issueId = (int) $issueRow->issue_id;
+				$issueIds[] = $issueId;
+				$publishedIssuesResult->next();
+			}
+
+			if (!empty($issueIds)) {
+				$publishedSubmissionsResult = $issueDao->retrieve(
+					'SELECT i.issue_id,
+						s.submission_id,
+						COALESCE(NULLIF(p.url_path, \'\'), CAST(s.submission_id AS CHAR(20))) AS article_best_id,
+						COALESCE(NULLIF(g.url_path, \'\'), CAST(g.galley_id AS CHAR(20))) AS galley_best_id
+					FROM issues i
+					LEFT JOIN custom_issue_orders o ON (o.issue_id = i.issue_id)
+					INNER JOIN publication_settings ps ON (
+						ps.setting_value = CAST(i.issue_id AS CHAR(20))
+						AND ps.setting_name = ?
+						AND ps.locale = \'\'
+					)
+					INNER JOIN publications p ON (p.publication_id = ps.publication_id)
+					INNER JOIN submissions s ON (s.current_publication_id = p.publication_id)
+					LEFT JOIN publication_galleys g ON (g.publication_id = p.publication_id)
+					WHERE i.journal_id = ?
+						AND i.published = 1
+						AND s.context_id = ?
+						AND s.status = ?
+					ORDER BY o.seq ASC, i.current DESC, i.date_published DESC, s.date_submitted DESC, g.seq ASC',
+					['issueId', (int) $journalId, (int) $journalId, (int) STATUS_PUBLISHED]
+				);
+
+				$publishedRowsByIssue = [];
+				while ($publishedSubmissionRow = $publishedSubmissionsResult->current()) {
+					$publishedRowsByIssue[$publishedSubmissionRow->issue_id][] = $publishedSubmissionRow;
+					$publishedSubmissionsResult->next();
+				}
+
+				foreach ($issueIds as $issueId) {
+					$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'issue', 'view', $issueId)));
+					$issueRows = $publishedRowsByIssue[(string) $issueId] ?? [];
+					$addedArticles = [];
+					foreach ($issueRows as $issueRow) {
+						$articleBestId = $issueRow->article_best_id;
+						$articleKey = (string) $issueRow->submission_id;
+
+						if (!isset($addedArticles[$articleKey])) {
+							$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'article', 'view', [$articleBestId])));
+							$addedArticles[$articleKey] = true;
+						}
+
+						if ($issueRow->galley_best_id !== null) {
+							$root->appendChild($this->_createUrlTree($doc, $request->url($journal->getPath(), 'article', 'view', [$articleBestId, $issueRow->galley_best_id])));
+						}
 					}
 				}
 			}
@@ -65,5 +110,3 @@ class SitemapHandler extends PKPSitemapHandler {
 	}
 
 }
-
-
